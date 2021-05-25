741bd9366383b71e058ae0b2ff31bdd4b4e59d21
choijinhyun <chjh0902@naver.com>
2021-05-25 21:05:51 +0900

    proj4 BE Proceeding

--- src/main/java/kr/or/connect/boostcourse/dao/ProductDao.java
+++ src/main/java/kr/or/connect/boostcourse/dao/ProductDao.java
@@ -1,9 +1,9 @@
 package kr.or.connect.boostcourse.dao;
 
-import static kr.or.connect.boostcourse.dao.ProductDaoSqls.SELECT_ALL;
-import static kr.or.connect.boostcourse.dao.ProductDaoSqls.SELECT_BY_CATEGORY;
-import static kr.or.connect.boostcourse.dao.ProductDaoSqls.SELECT_COUNT;
+import static kr.or.connect.boostcourse.dao.ProductDaoSqls.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -17,11 +17,13 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.boostcourse.dto.Product;
+import kr.or.connect.boostcourse.dto.ProductImages;
 
 @Repository
 public class ProductDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private RowMapper<Product> rowMapper = BeanPropertyRowMapper.newInstance(Product.class);
+	private RowMapper<ProductImages> rowMapperImages = BeanPropertyRowMapper.newInstance(ProductImages.class);
 	
 	public ProductDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -47,6 +49,12 @@ public class ProductDao {
 	/* 전체 product 갯수 */
 	public int selectCount() {
 		return jdbc.queryForObject(SELECT_COUNT, Collections.emptyMap(), Integer.class);
+	}
+	
+	public List<ProductImages> selectProductImages(int displayInfoId){
+		Map<String, Integer> params = new HashMap<>();
+		params.put("display_info_id", displayInfoId);
+		return jdbc.query(SELECT_PRODUCT_IMAGES, params, rowMapperImages);
 	}
 	
 }
--- src/main/java/kr/or/connect/boostcourse/dao/ProductDaoSqls.java
+++ src/main/java/kr/or/connect/boostcourse/dao/ProductDaoSqls.java
@@ -22,4 +22,24 @@ public class ProductDaoSqls {
 			+ "AND p.category_id = :category_id\n"
 			+ "ORDER BY p.id limit :start, :limit";
 	
+	public static final String SELECT_PRODUCT_PRICES = "SELECT pp.id AS product_price_id ,pp.product_id ,pp.price_type_name ,pp.price ,pp.discount_rate ,pp.create_date ,pp.modify_date \n"
+			+ "FROM product_price pp INNER JOIN display_info di ON pp.product_id = di.product_id \n"
+			+ "WHERE di.id = :display_info_id";
+	
+	public static final String SELECT_PRODUCT_IMAGES = "SELECT fi.content_type ,fi.create_date ,fi.delete_flag ,fi.id AS file_info_id ,fi.file_name ,fi.modify_date ,img.product_id ,img.id as product_image_id ,fi.save_file_name ,img.type \n"
+			+ "FROM product_image img INNER JOIN display_info di ON img.product_id = di.product_id\n"
+			+ "					      INNER JOIN file_info fi ON img.file_id = fi.id \n"
+			+ "WHERE di.id = :display_info_id \n"
+			+ "AND img.type IN ('ma', 'et')\n"
+			+ "LIMIT 2";
+	
+	public static final String SELECT_DISPLAY_INFO_IMAGE = "SELECT fi.content_type ,fi.create_date ,fi.delete_flag ,img.display_info_id ,img.id AS display_info_image_id, fi.id AS file_id ,fi.file_name , fi.modify_date ,fi.save_file_name \n"
+			+ "FROM display_info_image img INNER JOIN file_info fi ON img.file_id = fi.id \n"
+			+ "WHERE img.display_info_id = :display_info_id";
+	
+	public static final String SELECT_DISPLAY_INFO = "SELECT c.id AS category_id ,c.name ,di.tel AS telephone ,di.id AS display_info_id, di.*, p.content AS product_content, p.description AS product_description, p.event AS product_event, p.id AS product_id\n"
+			+ "FROM display_info di INNER JOIN product p ON di.product_id = p.id \n"
+			+ "					    INNER JOIN category c ON c.id = p.category_id \n"
+			+ "WHERE di.id = :display_info_id";
+	
 }
